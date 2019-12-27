e55cd39db47c4ae9c49439ddd6e4447db80c14d1
danibotcode <45088418+danibotcode@users.noreply.github.com>
2019-12-26 22:01:40 -0800

    Create Pick Primes-ll.rb

---
+++ Pick Primes-ll.rb
@@ -0,0 +1,22 @@
+def prime?(num)
+  if num < 2
+    return false
+  end
+
+  (2...num).each do |factor|
+    if num % factor == 0
+      return false
+    end
+  end
+
+  return true
+end
+
+def pick_primes(numbers)
+  return numbers.select { |num| prime?(num)}
+end
+
+print pick_primes([2, 3, 4, 5, 6]) #=> [2, 3, 5]
+puts
+print pick_primes([101, 20, 103, 2017]) #=> [101, 103, 2017]
+puts
